78ff9fd8dceefea6246a71d2c0264ffa8ed7d594
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-29 23:30:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,16 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-  if ((number%3==0) && (number%5!=0)) {
+  if ((number%3==0) && (number%5!=0)) 
     return "fizz"
-  }
-else if ((number%3!=0) && (number%5==0)) {
-  return "buzz"
-}
-else if ((number%3!=0) && (number%5!=0)) {
-  return "nil"  
-}
-else ((number%3==0) && (number%5==0)) {
-  return "fizzbuzz" 
-}
+  else if ((number%3!=0) && (number%5==0)) 
+    return "buzz"
+  else if ((number%3!=0) && (number%5!=0))
+    return "nil"  
+  else ((number%3==0) && (number%5==0)) 
+    return "fizzbuzz" 
+  end
 end
